2daa4b9e6c013fced15bb72c36d3eb73fbe42aa4
Graziely Tukozaki <grazielybasilio@hotmail.com>
2020-12-27 16:41:30 -0300

    Cobrinha cresce conforme come frutinha

--- JS/script.js
+++ JS/script.js
@@ -34,6 +34,7 @@ function drawnFood (){
 document.addEventListener('keydown',update);
 
 function update (event){
+
     if (event.keyCode == 37 && direction != "right") direction = "left";
     if (event.keyCode == 38 && direction != "down") direction = "up";
     if (event.keyCode == 39 && direction != "left") direction = "right";
@@ -42,15 +43,16 @@ function update (event){
 } 
 
 function iniciarJogo (){
+
     if (snake[0].x > 15 * box && direction == "right") snake [0].x = 0;
     if (snake[0].x < 0 && direction == "left") snake [0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down") snake [0].y = 0;
-    if (snake[0].y < 0 && direction == "up") snake [0].y =  16 * box;
+    if (snake[0].y < 0 && direction == "up") snake [0].y = 16 * box;
 
     criarBG ();
     criarCobrinha ();
     drawnFood ();
-    
+
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
@@ -59,7 +61,13 @@ function iniciarJogo (){
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y){
+        snake.pop ();
+    } else {
+        food.x = Math.floor(Math.random () * 15 + 1) * box;
+        food.y = Math.floor(Math.random () * 15 + 1) * box;
+    }
+
 
     let newHead = {
         x: snakeX,
