79cd3317252dbca1cacfeb6cfb7674f2b0bb2bfe
Graziely Tukozaki <grazielybasilio@hotmail.com>
2020-12-27 18:39:02 -0300

    Layout aprimorado e inserção de pontuação

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,8 @@
 let canvas = document.getElementById ("snake");
 let context = canvas.getContext("2d");
-let box = 32;
+let box = 30;
 let snake = [];
+
 snake [0] = {
     x: 8 * box,
     y: 8 * box,
@@ -14,20 +15,20 @@ let food = {
 }
 
 function criarBG (){
-    context.fillStyle = "lightgreen";
+    context.fillStyle = '#e4f2f7';
     context.fillRect (0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha (){
     for (i=0; i<snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = '#b6d3a8';
         context.fillRect(snake[i].x, snake[i].y,box,box);
     }
 
 }
 
 function drawnFood (){
-    context.fillStyle = "red";
+    context.fillStyle = '#ff574d';
     context.fillRect (food.x, food.y, box, box);
 }
 
@@ -44,10 +45,10 @@ function update (event){
 
 function iniciarJogo (){
 
-    if (snake[0].x > 15 * box && direction == "right") snake [0].x = 0;
-    if (snake[0].x < 0 && direction == "left") snake [0].x = 16 * box;
-    if (snake[0].y > 15 * box && direction == "down") snake [0].y = 0;
-    if (snake[0].y < 0 && direction == "up") snake [0].y = 16 * box;
+    if (snake[0].x > 15 * box ) snake [0].x = 0;
+    if (snake[0].x < 0 ) snake [0].x = 15 * box;
+    if (snake[0].y > 15 * box ) snake [0].y = 0;
+    if (snake[0].y < 0 ) snake [0].y = 15 * box;
 
     for (i= 1; i<snake.length; i++){
         if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
@@ -73,6 +74,7 @@ function iniciarJogo (){
     } else {
         food.x = Math.floor(Math.random () * 15 + 1) * box;
         food.y = Math.floor(Math.random () * 15 + 1) * box;
+        document.getElementById("marcaPonto").innerHTML = snake.length;
     }
 
 
@@ -84,4 +86,4 @@ function iniciarJogo (){
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 140);
