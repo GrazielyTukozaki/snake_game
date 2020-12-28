3057cccff26441c1c6a99bf3d6d7b0b2f3bbbb36
Graziely Tukozaki <grazielybasilio@hotmail.com>
2020-12-28 14:09:09 -0300

    Adição funcionalidades de pausa e reinicio do jogo

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,8 @@ let canvas = document.getElementById ("snake");
 let context = canvas.getContext("2d");
 let box = 30;
 let snake = [];
+let controle = "andamento";
+let popup = document.getElementById ("pop_up").style.zIndex = "-1"
 
 snake [0] = {
     x: 8 * box,
@@ -40,11 +42,17 @@ function update (event){
     if (event.keyCode == 38 && direction != "down") direction = "up";
     if (event.keyCode == 39 && direction != "left") direction = "right";
     if (event.keyCode == 40 && direction != "up") direction = "down";
-
+    if (event.keyCode == 32 && controle == "andamento") controle = "pausa";
+    else if (event.keyCode == 32 && controle == "pausa") controle = "andamento"; 
+    if (controle == "andamento") document.getElementById ("pop_up").style.zIndex = "-1";
+    if (controle == "pausa") document.getElementById ("pop_up").style.zIndex = "1";
 } 
+
 
 function iniciarJogo (){
 
+    if (controle == "andamento"){
+    
     if (snake[0].x > 15 * box ) snake [0].x = 0;
     if (snake[0].x < 0 ) snake [0].x = 15 * box;
     if (snake[0].y > 15 * box ) snake [0].y = 0;
@@ -53,7 +61,8 @@ function iniciarJogo (){
     for (i= 1; i<snake.length; i++){
         if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval (jogo);
-            alert ('Game Over');
+            food = [];
+            document.getElementById ("gameOver").style.zIndex = "1";
         }
     }
 
@@ -84,6 +93,24 @@ function iniciarJogo (){
     }
 
     snake.unshift(newHead);
+    } 
+         
+
+}
+
+function reiniciar (){
+    snake = [];
+    snake [0] = {
+        x: 8 * box,
+        y: 8 * box,
+    }
+    document.getElementById("marcaPonto").innerHTML = 0;
+    document.getElementById ("gameOver").style.zIndex = "-1";
+    jogo = setInterval(iniciarJogo, 140);
+    food = {
+        x: Math.floor(Math.random () * 15 + 1) * box,
+        y: Math.floor(Math.random () * 15 + 1) * box,
+    }
 }
 
 let jogo = setInterval(iniciarJogo, 140);
